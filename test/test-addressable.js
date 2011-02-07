90af461b052ae40243d3434934cc426ce4a2036d
Robert Sköld <robert@publicclass.se>
2011-02-07 15:10:55 -0500

    Updated version. Now with a working URI.extract method.

--- test/test-addressable.js
+++ test/test-addressable.js
@@ -11,216 +11,94 @@ var text = [
   "What happens with urls with spaces? http://google.com/search?q=with a space"
 ].join(" \n")
 
-console.log(text)
+exports["an empty string"] = function(){
+  assert.ok( addressable.parse("") instanceof addressable.URI, "should be an URI instance" )
+}
 
-vows.describe( "An Addressable URI" ).addBatch({
-  "an empty string": {
-    topic: addressable.parse(""),
+exports["an (non-auth) HTTP URI"] = function(){
+  var http = addressable.parse("http://www.example.com/hej/test.html?query=bula&another=bula#anchored")
+  assert.equal(http.scheme,"http","should have 'http' scheme")
+  assert.equal(http.host,"www.example.com","should have a full host.")
+  assert.equal(http.host,http.authority,"should have equal authority and host.")
+  assert.equal(http.path,"/hej/test.html","should have a path.")
+  assert.equal(http.querystring,"query=bula&another=bula","should have a querystring.")
+  assert.eql(http.query,{query: "bula",another: "bula"},"should have a query object.")
+  assert.equal(http.fragment,"anchored","should have a fragment.")
+  var q = http.query
+  q.more = "bula";
+  http.query = q;
+  assert.equal(http.querystring,"query=bula&another=bula&more=bula","should be able to modify the query object.")
+}
+
+exports["an (authorized) IP HTTP URI"] = function(){
+  var ip = addressable.parse("https://user:pass@123.123.123.123:81/path/../a")
+  assert.equal("123.123.123.123",ip.host,"should have IP address as host.")
+  assert.equal(ip.scheme,"https","should have 'https' scheme.")
+  assert.notEqual(ip.host,ip.authority,"should not have the same authority as host.")
+  assert.equal( ip.userinfo , "user:pass","should have a userinfo")
+  assert.equal( ip.username , "user", "should have a username" )
+  assert.equal( ip.password , "pass", "should have a password" )
+  assert.equal( ip.port , 81, "should have a non-standard port" )
+  assert.equal( ip.path , "/a" , "should have a normalized path" )  
+}
+
+exports["a relative file URI"] = function(){
+  var file = addressable.parse("file://a/../README");
+  assert.equal( file.scheme , "file", "should have a 'file' scheme." )
+  assert.ok( file.isRelative(), "should be relative." )
+  assert.isFalse( file.isAbsolute(), "should not be absolute." )
+  assert.equal( file.path , "README", "should have a normalized path." )
+}
   
-    "should be an URI instance.": function(uri){
-      assert.instanceOf( uri , addressable.URI );
-    }
-  },
+exports["a relative file without scheme"] = function(){
+  var file = addressable.parse("a/../README")
+  assert.equal( file.scheme , undefined, "should have no scheme." )
+  assert.ok( file.isRelative() , "should be relative." )
+  assert.isFalse( file.isAbsolute() , "should not be absolute." )
+  assert.equal( file.path , "README" , "should have a normalized path." )
+}
+
+exports["an absolute file URI"] = function(){
+  var file = addressable.parse("file:///etc/test/.././hosts")
+  assert.equal( file.scheme , "file", "should have a 'file' scheme." )
+  assert.ok( !file.isRelative(), "should not be relative." )
+  assert.ok( file.isAbsolute(), "should be absolute." )
+  assert.equal( file.path , "/etc/hosts", "should have a normalized path." )
+}
+
+exports["an absolute file without a scheme"] = function(){
+  var file = addressable.parse("/etc/test/.././hosts");
+  assert.equal( file.scheme , undefined, "an absolute file without a scheme." )
+  assert.ok( !file.isRelative() , "should not be relative." )
+  assert.ok( file.isAbsolute() , "should be absolute." )
+  assert.equal( file.path , "/etc/hosts" , "should have a normalized path." )
+}
+
+exports["extracted from a text"] = function(){
+  var arr = addressable.extract(text);
+  assert.isArray(arr);
+  arr.forEach(function(uri){
+    assert.instanceOf(uri,addressable.URI)
+  })
+}
+
+exports["extracted from a text without urls"] = function(){
+  var arr = addressable.extract("a linkless text");
+  assert.isArray(arr)
+  assert.isEmpty(arr)
+}
+
+exports["extracted with a replace function"] = function(){
+  var txt = addressable.extract(text,function(uri){
+    assert.ok( uri instanceof addressable.URI );
+    return "##URL##" 
+  })
+  assert.notEqual(text,txt)
+  assert.includes(txt,"##URL##")
+  assert.isEmpty(addressable.extract(txt))
+}
   
-  "an (non-auth) HTTP URI": {
-    topic: addressable.parse("http://www.example.com/hej/test.html?query=bula&another=bula#anchored"),
-    
-    "should have 'http' scheme": function(http){
-      assert.equal(http.scheme,"http");
-    },
-    
-    "should have a full host.": function(http){
-      assert.equal(http.host,"www.example.com");
-    },
-
-    "should have equal authority and host.": function(http){
-      assert.equal(http.host,http.authority);
-    },
-
-    "should have a path.": function(http){
-      assert.equal(http.path,"/hej/test.html");
-    },
-
-    "should have a querystring.": function(http){
-      assert.equal(http.querystring,"query=bula&another=bula");
-    },
-
-    "should have a query object.": function(http){
-      var correct = {
-        query: "bula",
-        another: "bula"
-      }
-      assert.deepEqual(http.query,correct);
-    },
-
-    "should have a fragment.": function(http){
-      assert.equal(http.fragment,"anchored");
-    },
-
-    "should be able to modify the query object.": function(http){
-      var q = http.query;
-      q.more = "bula";
-      http.query = q;
-      assert.equal(http.querystring,"query=bula&another=bula&more=bula");
-    }
-  },
-  
-  "a (secure, authorized) IP HTTP URI": {
-    topic: addressable.parse("https://user:pass@123.123.123.123:81/path/../a"),
-
-    "should have IP address as host.": function(ip){
-      assert.equal("123.123.123.123",ip.host);
-    },
-    
-    "should have 'https' scheme.": function(ip){
-      assert.equal(ip.scheme,"https");
-    },
-    
-    "should not have the same authority as host.": function(ip){
-      assert.notEqual(ip.host,ip.authority);
-    },
-
-    "should have a userinfo.": function(ip){
-      assert.isNotNull( ip.userinfo );
-      assert.equal( ip.userinfo , "user:pass" );
-    },
-    
-    "should have a username.": function(ip){
-      assert.isNotNull( ip.username );
-      assert.equal( ip.username , "user" );
-    },
-
-    "should have a password.": function(ip){
-      assert.isNotNull( ip.password );
-      assert.equal( ip.password , "pass" );
-    },
-    
-    "should have a non-standard port.": function(ip){
-      assert.equal( ip.port , 81 );
-    },
-    
-    "should have a normalized path.": function(ip){
-      assert.equal( ip.path , "/a" );
-    }
-  },
-  
-  "a relative file URI": {
-    topic: addressable.parse("file://a/../README"),
-    
-    "should have a 'file' scheme.": function(file){
-      assert.equal( file.scheme , "file" );
-    },
-    
-    "should be relative.": function(file){
-      assert.isTrue( file.isRelative() );
-    },
-    
-    "should not be absolute.": function(file){
-      assert.isFalse( file.isAbsolute() );
-    },
-    
-    "should have a normalized path.": function(file){
-      assert.equal( file.path , "README" );
-    }
-  },
-  
-  "a relative file without scheme": {
-    topic: addressable.parse("a/../README"),
-
-    "should have a 'file' scheme.": function(file){
-      assert.equal( file.scheme , undefined );
-    },
-
-    "should be relative.": function(file){
-      assert.isTrue( file.isRelative() );
-    },
-
-    "should not be absolute.": function(file){
-      assert.isFalse( file.isAbsolute() );
-    },
-
-    "should have a normalized path.": function(file){
-      assert.equal( file.path , "README" );
-    }
-  },
-  
-  "an absolute file URI": {
-    topic: addressable.parse("file:///etc/test/.././hosts"),
-
-    "should have a 'file' scheme.": function(file){
-      assert.equal( file.scheme , "file" );
-    },
-
-    "should not be relative.": function(file){
-      assert.isFalse( file.isRelative() );
-    },
-
-    "should be absolute.": function(file){
-      assert.isTrue( file.isAbsolute() );
-    },
-
-    "should have a normalized path.": function(file){
-      assert.equal( file.path , "/etc/hosts" );
-    }
-  },
-  
-  "an absolute file without a scheme": {
-    topic: addressable.parse("/etc/test/.././hosts"),
-
-    "should have no 'file' scheme.": function(file){
-      assert.equal( file.scheme , undefined );
-    },
-
-    "should not be relative.": function(file){
-      assert.isFalse( file.isRelative() );
-    },
-
-    "should be absolute.": function(file){
-      assert.isTrue( file.isAbsolute() );
-    },
-
-    "should have a normalized path.": function(file){
-      assert.equal( file.path , "/etc/hosts" );
-    }
-  },
-  
-  "extracted from a text": {
-    topic: addressable.extract(text),
-    
-    "should return an array of URIs.": function(arr){
-      assert.isArray(arr);
-      arr.forEach(function(uri){
-        assert.instanceOf(uri,addressable.URI)
-      })
-    }
-  },
-  
-  "extracted from a text without urls": {
-    topic: addressable.extract("a linkless text"),
-    
-    "should return an empty array.": function(arr){
-      assert.isArray(arr)
-      assert.isEmpty(arr)
-    }
-  },
-  
-  "extracted with a replace function": {
-    topic: addressable.extract(text,function(uri){ return uri.host }),
-    
-    "should not match the original text.": function(txt){
-      assert.notEqual(text,txt);
-    },
-    
-    "should not contain any urls.": function(txt){
-      assert.isEmpty(addressable.extract(txt))
-    }
-  },
-  
-  "extracted with an empty replace function": {
-    topic: addressable.extract(text,function(uri){}),
-
-    "should match the original text.": function(txt){
-      assert.equal(text,txt);
-    }
-  }
-}).run()
+exports["extracted with an empty replace function"] = function(){
+  var txt = addressable.extract(text,function(uri){})
+  assert.equal(text,txt)
+}
